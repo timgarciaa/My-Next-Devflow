4cb8c067cfd58195cb2bbb1dfcfa20fc0adcc542
Tim Garcia <timgarciaa@gmail.com>
2024-01-10 21:59:51 +0800

    Add Clerk.js authentication and middleware

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { UserButton } from "@clerk/nextjs";
 
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      Home Page
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 
 export default HomePage;
