d5c5badb0482fd60616d0e7ffb852dbb142ab494
Tim Garcia <timgarciaa@gmail.com>
2024-01-10 20:52:30 +0800

    Add authentication and home pages

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Next.js 14</h1>
-      <h2 className="h2-bold">Next.js 14</h2>
-      <h2 className="h3-bold">Next.js 14</h2>
-    </div>
-  );
-}
