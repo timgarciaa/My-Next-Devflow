7888eecb0fe82919d4989c9f7cb6d0775d7876c4
Tim Garcia <timgarciaa@gmail.com>
2024-01-11 22:36:59 +0800

    Update home page

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -4,6 +4,51 @@ import { Button } from "@/components/ui/button";
 import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
 import Filter from "@/components/shared/Filter";
 import { HomePageFilters } from "@/constants/filters";
+import HomeFilters from "@/components/home/HomeFilters";
+import QuestionCard from "@/components/cards/QuestionCard";
+import NoResult from "@/components/shared/NoResult";
+
+const questions = [
+  {
+    _id: "1",
+    title: "Cascading Deletes in SQLAlchemy",
+    tags: [
+      { _id: "1", name: "python" },
+      { _id: "2", name: "sql" },
+    ],
+    author: { _id: "1", name: "John Doe", picture: "" },
+    upvotes: 10,
+    views: 602399,
+    answers: [{}],
+    createdAt: new Date("2021-08-01T12:00:00.000Z"),
+  },
+  {
+    _id: "2",
+    title: "How to use React Router",
+    tags: [
+      { _id: "1", name: "react" },
+      { _id: "2", name: "javascript" },
+    ],
+    author: { _id: "1", name: "John Doe", picture: "" },
+    upvotes: 10,
+    views: 1000000,
+    answers: [{}],
+    createdAt: new Date("2021-08-01T12:00:00.000Z"),
+  },
+  {
+    _id: "3",
+    title: "How to use React Router",
+    tags: [
+      { _id: "1", name: "react" },
+      { _id: "2", name: "javascript" },
+    ],
+    author: { _id: "1", name: "John Doe", picture: "" },
+    upvotes: 10,
+    views: 100,
+    answers: [{}],
+    createdAt: new Date("2021-08-01T12:00:00.000Z"),
+  },
+];
 
 const HomePage = () => {
   return (
@@ -38,8 +83,37 @@ const HomePage = () => {
         <Filter
           filters={HomePageFilters}
           otherClasses="min-h-[56px] sm:min-w-[170px]"
-          containerClasses="hidden md:flex"
+          containerClasses="hidden max-md:flex"
         />
+      </div>
+
+      <HomeFilters />
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There's no question to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the
+          discussion. our query could be the next big thing others learn from. Get
+          involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
       </div>
     </>
   );
