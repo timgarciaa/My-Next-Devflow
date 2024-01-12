bcdfafcee255f82e7cb2bb3f5a8726bcf70baa67
Tim Garcia <timgarciaa@gmail.com>
2024-01-12 11:36:56 +0800

    Add new files and update existing files

--- src/app/(root)/ask-question/page.tsx
+++ src/app/(root)/ask-question/page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Question from "@/components/forms/Question";
 
 const AskQuestionPage = () => {
-  return <div>AskQuestionPage</div>;
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
+      <div className="mt-9">
+        {" "}
+        <Question />
+      </div>
+    </div>
+  );
 };
 
 export default AskQuestionPage;
--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -21,7 +21,7 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
     ) {
       document.documentElement.classList.add("dark");
     } else {
-      document.documentElement.classList.remove("dark");;
+      document.documentElement.classList.remove("dark");
     }
   };
 
