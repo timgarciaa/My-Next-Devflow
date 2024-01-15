df20b6840496a8e4df8b309623fe6b88b3dfa446
Tim Garcia <timgarciaa@gmail.com>
2024-01-15 11:48:06 +0800

    Refactor auth layout and sign-in/sign-up pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-type Props = {
-  children: React.ReactNode;
-};
-
-const Layout = ({ children }: Props) => {
-  return <div>{children}</div>;
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="flex min-h-screen w-full items-center justify-center">
+      {children}
+    </main>
+  );
 };
 
 export default Layout;
