04ea39d5b396d13d78ddfa964fd5a6606e5ab29f
Tim Garcia <timgarciaa@gmail.com>
2024-01-17 12:10:26 +0800

    Refactor HomePage to fetch questions based on search parameters

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -7,52 +7,15 @@ import { HomePageFilters } from "@/constants/filters";
 import HomeFilters from "@/components/home/HomeFilters";
 import QuestionCard from "@/components/cards/QuestionCard";
 import NoResult from "@/components/shared/NoResult";
-// import { getQuestions } from "@/lib/actions/question.action";
+import { getQuestions } from "@/lib/actions/question.action";
+import { SearchParamsProps } from "@/types";
 
-const questions = [
-  {
-    _id: "1",
-    title: "Cascading Deletes in SQLAlchemy",
-    tags: [
-      { _id: "1", name: "python" },
-      { _id: "2", name: "sql" },
-    ],
-    author: { _id: "1", name: "John Doe", picture: "" },
-    upvotes: 10,
-    views: 602399,
-    answers: [{}],
-    createdAt: new Date("2021-08-01T12:00:00.000Z"),
-  },
-  {
-    _id: "2",
-    title: "How to use React Router",
-    tags: [
-      { _id: "1", name: "react" },
-      { _id: "2", name: "javascript" },
-    ],
-    author: { _id: "1", name: "John Doe", picture: "" },
-    upvotes: 10,
-    views: 1000000,
-    answers: [{}],
-    createdAt: new Date("2021-08-01T12:00:00.000Z"),
-  },
-  {
-    _id: "3",
-    title: "How to use React Router",
-    tags: [
-      { _id: "1", name: "react" },
-      { _id: "2", name: "javascript" },
-    ],
-    author: { _id: "1", name: "John Doe", picture: "" },
-    upvotes: 10,
-    views: 100,
-    answers: [{}],
-    createdAt: new Date("2021-08-01T12:00:00.000Z"),
-  },
-];
-
-export default async function HomePage() {
-  // const result = await getQuestions();
+export default async function HomePage({ searchParams }: SearchParamsProps) {
+  const result = await getQuestions({
+    searchQuery: searchParams.q,
+    filter: searchParams.filter,
+    page: searchParams.page ? +searchParams.page : 1,
+  });
   return (
     <>
       <div
@@ -92,8 +55,8 @@ export default async function HomePage() {
       <HomeFilters />
 
       <div className="mt-10 flex w-full flex-col gap-6">
-        {questions.length > 0 ? (
-          questions.map((question) => (
+        {result.questions.length > 0 ? (
+          result.questions.map((question) => (
             <QuestionCard
               key={question._id}
               _id={question._id}
