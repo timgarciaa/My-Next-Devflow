6fbdb8c7719b57ff1b295df4d41f1a8b96919502
Tim Garcia <timgarciaa@gmail.com>
2024-02-01 21:42:36 +0800

    Add tag page component and implement fetching questions by tag ID

--- src/lib/actions/tag.action.ts
+++ src/lib/actions/tag.action.ts
@@ -1,9 +1,16 @@
+/* eslint-disable no-unused-vars */
 "use server";
 
 import { connectToDatabase } from "@/lib/mongoose";
-import { GetAllTagsParams, GetTopInteractedTagsParams } from "@/lib/actions/shared.types";
+import {
+  GetAllTagsParams,
+  GetQuestionsByTagIdParams,
+  GetTopInteractedTagsParams,
+} from "@/lib/actions/shared.types";
 import User from "@/database/user.model";
-import Tag from "@/database/tag.model";
+import Tag, { ITag } from "@/database/tag.model";
+import { FilterQuery } from "mongoose";
+import Question from "@/database/question.model";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -31,8 +38,44 @@ export async function getAllTags(params: GetAllTagsParams) {
 
     const tags = await Tag.find({});
 
-    return {tags};
-  } catch(e) {
+    return { tags };
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
+
+export async function getQuestionsByTagId(params: GetQuestionsByTagIdParams) {
+  try {
+    connectToDatabase();
+
+    const { tagId, page = 1, pageSize = 10, searchQuery } = params;
+
+    const tagFilter: FilterQuery<ITag> = { _id: tagId };
+
+    const tag = await Tag.findOne(tagFilter).populate({
+      path: "questions",
+      model: Question,
+      match: searchQuery
+        ? { title: { $regex: searchQuery, $options: "i" } }
+        : {},
+      options: {
+        sort: { createdAt: -1 },
+      },
+      populate: [
+        { path: "tags", model: Tag, select: "_id name" },
+        { path: "author", model: User, select: "_id clerkId name picture" },
+      ],
+    });
+
+    if (!tag) {
+      throw new Error("Tag not found");
+    }
+
+    const questions = tag.questions;
+
+    return { tagTitle: tag.name, questions };
+  } catch (e) {
     console.log(e);
     throw e;
   }
